8e5b4aa40e66b750ca9f555dedcbca6697f5f663
Aleksandr Gyshchak <sasha991@ukr.net>
2019-01-15 00:57:27 +0200

    v 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { interval, timer, Subject, of, empty, BehaviorSubject, fromEvent, merge } from 'rxjs';
+import { takeUntil, switchMap, withLatestFrom, filter, map, debounceTime, mapTo, startWith, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rxjs-pause-timer-angular7';
+
+
+export class AppComponent implements AfterViewInit {
+  @ViewChild('btnStartStop') btnStartStop: ElementRef;
+  @ViewChild('btnPause') btnPause: ElementRef;
+  @ViewChild('btnReset') btnReset: ElementRef;
+
+  timerValue: Number = 0;
+  resetTimer$ = new Subject();
+
+  constructor() { }
+
+  ngAfterViewInit() {
+
+    const pause$ = fromEvent(this.btnPause.nativeElement, 'click')
+      .pipe(
+        debounceTime(300),
+        mapTo('pause')
+      );
+
+    const resume$ = fromEvent(this.btnStartStop.nativeElement, 'click')
+      .pipe(
+        mapTo('resume')
+      );
+
+    const reset$ = fromEvent(this.btnReset.nativeElement, 'click');
+    reset$.subscribe(() => {
+      this.resetTimer$.next();
+      this.timerValue = 0;
+      this.initTimer(pause$, resume$);
+    });
+
+    this.initTimer(pause$, resume$);
+  }
+  private initTimer(pause$, resume$) {
+
+    merge(pause$, resume$)
+      .pipe(
+        startWith(0),
+        switchMap(val => {
+          return (val === 'pause') ? empty() : interval(1000).pipe(mapTo(1));
+        }),
+        takeUntil(this.resetTimer$),
+        scan((accumulator, curr) => {
+          console.log(accumulator, curr);
+          return accumulator + curr;
+        })
+      )
+      .subscribe(
+        timerVal => this.timerValue = timerVal
+      );
+  }
+
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondsToTimePipe } from './shared/pipes/seconds-to-time.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondsToTimePipe
   ],
   imports: [
     BrowserModule,
